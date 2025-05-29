8cadcf724b35a23de21a5c8fd8a76b7657942aed
ferguskidd <45766226+FergusKidd@users.noreply.github.com>
2025-05-29 16:20:42 +0100

    change to worker

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@
 pip install -r requirements.txt
 
 # Start the FastAPI application with Gunicorn and Uvicorn worker
-gunicorn --config gunicorn.conf.py main:app 
+gunicorn application:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 
